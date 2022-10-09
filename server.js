86c8a857250499503c278dcb37c9178cb46cc2dc
Abdelrahman Hany <34286982+Karitori@users.noreply.github.com>
2022-10-09 14:31:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get("/init", function (req, res) {
 	res.send("Working Perfectly Fine ^_^");
 });
 
+app.get("/init2", function (req, res) {
+	res.send("Working Perfectly Fine2 ^_^");
+});
+
 app.listen(3000, function () {
 	console.log("[OK] = HTTP Server listening on: http://localhost:3000");
 });
